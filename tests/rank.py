faa14c65d1bec878725a650a0c95261ec70a72d6
zwergon <zwergon@free.fr>
2025-11-27 16:28:40 +0000

    make sbatch rank running on gpu.zwergon.fr

--- tests/rank.py
+++ tests/rank.py
@@ -5,37 +5,37 @@ import torch.distributed as dist
 from drp.utils.hostlist import expand_hostlist
 
 def main():
-    for k,v in os.environ.items():
-        if "SLURM" in k or 'NCCL' in k:
-            print(k, v)
+    
    
     # Récupère les variables Slurm
     rank = int(os.environ['SLURM_PROCID'])  # Rank local sur le nœud
-    local_rank = int(os.environ['SLURM_LOCALID'])  # Identique à SLURM_PROCID
     world_size = int(os.environ['SLURM_NTASKS'])  # Nombre total de processus
 
-    hostnames = expand_hostlist(os.environ['SLURM_JOB_NODELIST'])
+
+    if rank == 0:
+        for k,v in os.environ.items():
+            if "SLURM" in k or 'NCCL' in k:
+                print(k, v)
 
     # Adresse IP du nœud maître (premier nœud de la liste)
-    master_addr = socket.gethostbyname(hostnames[0])
-
+    master_addr = os.environ['MASTER_ADDR']
+    master_port = os.environ['MASTER_PORT']
+    backend = os.environ['BACKEND']
     # Initialise le backend distribué (NCCL pour GPU, GLOO pour CPU)
-    os.environ['MASTER_ADDR'] = master_addr
-    os.environ['MASTER_PORT'] = '12345'  # Port arbitraire
+     
     init_str = f'tcp://{os.environ["MASTER_ADDR"]}:{os.environ["MASTER_PORT"]}'
 
-    print(init_str)
+    print(f"Rank {rank} attempting to connect to {init_str}", flush=True)
 
-    
     dist.init_process_group(
-        backend='nccl',  # ou 'gloo' pour CPU
+        backend=backend,  # ou 'gloo' pour CPU
         init_method=init_str,
         rank=rank,
         world_size=world_size
     )
 
     # Exemple d'utilisation du rank
-    print(f"Hello from rank {rank} of {world_size} (local rank: {local_rank})")
+    print(f"Hello from rank {rank} of {world_size}")
 
     # Ton code distribué ici...
     dist.barrier()  # Synchronisation
